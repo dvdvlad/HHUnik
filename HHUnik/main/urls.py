efd7d52cdc94304842076b460c249ac01dc6052f
dvd <vladvd2341@gmail.com>
2026-04-26 13:16:26 +0400

    made link to hr account page

--- HHUnik/main/urls.py
+++ HHUnik/main/urls.py
@@ -3,4 +3,5 @@ from . import views
 app_name = 'main'  
 urlpatterns = [
     path("", views.Main.as_view(), name="index"),
+    path("account", views.hrAccountPage.as_view(), name="hrAccountPage"),
 ]
--- HHUnik/main/views.py
+++ HHUnik/main/views.py
@@ -3,3 +3,5 @@ from django.views.generic.base import TemplateView
 
 class Main(TemplateView):
     template_name="base.html"
+class hrAccountPage(TemplateView):
+    template_name="main/hrPage.html"
